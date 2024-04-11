15503e65f6c618d9f2d5476c0b9d1ba5c9eb4beb
Kalyani1102 <94071212+Kalyani1102@users.noreply.github.com>
2024-04-12 02:27:23 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,127 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './Login.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const FormValidationExample = () => {
+
+  const [formData, setFormData] = useState({
+    username: '',
+    email: '',
+    phone: '',
+    password: '',
+    confirmPassword: ''
+  })
+
+  const [errors, setErrors] = useState({})
+
+  const handleChange = (e) => {
+    const {name, value} = e.target;
+    setFormData({
+        ...formData, [name] : value
+    })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const validationErrors = {}
+    if(!formData.username.trim()) {
+        validationErrors.username = "username is required"
+    }
+
+    if(!formData.email.trim()) {
+        validationErrors.email = "email is required"
+    } else if(!/\S+@\S+\.\S+/.test(formData.email)){
+        validationErrors.email = "email is not valid"
+    }
+
+    if(!formData.email.trim()) {
+      validationErrors.email = "email is required"
+  } else if(!/\S+@\S+\.\S+/.test(formData.email)){
+      validationErrors.email = "email is not valid"
+  }
+
+  if(!formData.phone.trim()) {
+    validationErrors.phone = "Phonenumber is required"
+} else if(!/^\+?[1-9]\d{1,14}$/.test(formData.phone)){
+    validationErrors.phone = "Phonenumber is not valid"
 }
+  
+    if(!formData.password.trim()) {
+        validationErrors.password = "password is required"
+    } else if(formData.password.length < 8){
+        validationErrors.password = "password should be at least 8 char"
+    }
 
-export default App;
+    if(formData.confirmPassword !== formData.password) {
+        validationErrors.confirmPassword = "password not matched"
+    }
+
+    setErrors(validationErrors)
+
+    if(Object.keys(validationErrors).length === 0) {
+        alert("Form Submitted successfully")
+    }
+
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className='login'>Login Form</div>
+      <div>
+        <label>Username:</label>
+        <input
+          type="text"
+          name="username"
+          placeholder='username'  
+          autoComplete='off'  
+          onChange={handleChange}   
+        />
+          {errors.username && <span>{errors.username}</span>}  
+      </div>
+      <div>
+        <label>Email:</label>
+        <input
+          type="email"
+          name="email"
+          placeholder='example@gmail.com'
+          autoComplete='off'
+          onChange={handleChange} 
+        />
+          {errors.email && <span>{errors.email}</span>}  
+      </div>
+      <div>
+        <label>Phone Number:</label>
+        <input
+          type="phone"
+          name="phone"
+          maxLength="10"
+          placeholder='+91XXXXXXXX'
+          onChange={handleChange} 
+        />
+          {errors.phone && <span>{errors.phone}</span>}  
+      </div>
+      <div>
+        <label>Password:</label>
+        <input
+          type="password"
+          name="password"
+          placeholder='******'
+          onChange={handleChange} 
+        />
+          {errors.password && <span>{errors.password}</span>}  
+      </div>
+      <div>
+        <label>Confirm Password:</label>
+        <input
+          type="password"
+          name="confirmPassword"
+          placeholder='******'
+          onChange={handleChange} 
+        />
+          {errors.confirmPassword && <span>{errors.confirmPassword}</span>}  
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default FormValidationExample;
